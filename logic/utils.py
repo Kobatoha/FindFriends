0af8e83230380a16e20efb52be0ef322cbade877
kobatoha <kobatoha@gmail.com>
2024-12-14 22:17:01 +0300

    update move_to, move_mouse and get_mouse_position

--- logic/utils.py
+++ logic/utils.py
@@ -7,13 +7,32 @@ import win32con
 import win32api
 
 
+def left_click() -> None:
+    pyautogui.leftClick()
+
+
+def get_mouse_position(hwnd):
+    x, y = pyautogui.position()
+
+    rect = win32gui.GetWindowRect(hwnd)
+    x0, y0, x1, y1 = rect
+
+    x_rel = x - x0
+    y_rel = y - y0
+
+    x_rel = max(0, min(x_rel, x1 - x0))
+    y_rel = max(0, min(y_rel, y1 - y0))
+
+    return x_rel, y_rel
+
+
 def get_window_hwnd_by_title(title: str) -> int:
     windows = gw.getWindowsWithTitle(title)
     windows_hwnd = []
     for window in windows:
         print(f"Window Title: {window.title}, Window ID: {window._hWnd}")
         windows_hwnd.append(window._hWnd)
-    return windows_hwnd[0]
+    return windows_hwnd
 
 
 def get_focus_window(hwnd: int, title: str) -> None:
@@ -41,19 +60,20 @@ def get_window_size(hwnd):
     return width, height
 
 
-def move_mouse(hwnd, x, y):
+def move_mouse(hwnd, x=0, y=0):
     rect = win32gui.GetWindowRect(hwnd)
     x0, y0 = rect[2], rect[3]
 
     pyautogui.moveTo(x0 - x, y0 - y, 0.25)
+    print(x0 - x, y0 - y)
 
 
-if __name__ == "__main__":
-    title = '4game'
-    hwnd = get_window_hwnd_by_title(title)
-    if not check_active_window(hwnd, title):
-        get_focus_window(hwnd, title)
-        time.sleep(1)
-    else:
-        time.sleep(1)
-        move_and_click_game_button(hwnd)
+def move_to(hwnd, x, y):
+    rect = win32gui.GetWindowRect(hwnd)
+    x0, y0 = rect[0], rect[1]
+
+    target_x = x0 + x
+    target_y = y0 + y
+
+    pyautogui.moveTo(target_x, target_y, duration=0.25)
+    print(target_x, target_y)
